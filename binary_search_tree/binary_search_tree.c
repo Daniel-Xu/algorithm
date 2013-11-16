0106fe445aebff23318f7871c2dab5abd07e5d9a
Daniel-Xu <xwd.forever@gmail.com>
2013-11-16 00:13:26 -0500

    insertion

---
+++ binary_search_tree/binary_search_tree.c
@@ -0,0 +1,66 @@
+#include "binary_search_tree.h"
+#include <stdlib.h>
+
+static link make_node(int val)
+{
+  link p = malloc(sizeof(node));
+  p->val = val;
+  p->left = p->right = NULL:
+  p->parent = NULL;
+
+  return p;
+}
+
+link search(link t, int val)
+{
+   
+}
+
+//recursive version
+link insert(link t, int val)
+{
+  if(!t)
+    return make_node(val);
+
+  if(key > t->val) {
+    t->right = insert(t->right, val);
+    t->right->parent = t;
+  } else {
+    t->left = insert(t->left, val);
+    t->left->parent = t;
+  }
+  
+  return t;
+}
+
+//minimum
+link tree_mininum(link t)
+{
+
+}
+
+//maximum
+link tree_maximum(link t)
+{
+
+}
+
+//like sorted array, which one is the next one.
+link node_successor(link t)
+{
+
+}
+
+//transplant is used to change the subtree's parent
+void transplant(link t, link src, link subt)
+{
+
+
+}
+
+//delete
+link delete(link t, int val)
+{
+
+}
+
--- binary_search_tree/binary_search_tree.h
+++ binary_search_tree/binary_search_tree.h
@@ -1,6 +1,12 @@
 #ifndef BINARY_SEARCH_TREE_H_BFKVTIXN
 #define BINARY_SEARCH_TREE_H_BFKVTIXN
 
+typedef struct node{ 
+  int val;
+  struct node parent;
+  struct node left;
+  struct node right;
+}node, *link;
 
 
 #endif /* end of include guard: BINARY_SEARCH_TREE_H_BFKVTIXN */
