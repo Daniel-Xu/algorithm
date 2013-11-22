f85d43ab5399ff1ae695be38523d9be42cb3dcc9
Daniel-Xu <xwd.forever@gmail.com>
2013-11-22 01:39:16 -0500

    remeber when you want compile,
    tg=1_1.cpp make

--- exec/1_1.cpp
+++ exec/1_1.cpp
@@ -1,35 +1,75 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-bool isUnique(string str)
+// if it's ascii
+bool ipUnique1(string str)
 {
-  bool *p = new bool[200]();
+  bool a[256];
+  memset(a, 0, sizeof(a));
+  int len = str.length();
 
-  for(int i=0; i < str.length(); ++i) {
+  for(int i=0; i < len; ++i) {
     //char val = "" 
-    if (str.at(i))
-    {
-      return false;
-    } else {
-      str.at(i) = true; 
-    }
+    int p = (int)str[i];
+    if(a[p]) return false;
+    a[p] = true;
   }
+  return true;
+}
 
+//better for space
+bool ipUnique2(string str)
+{
+  int a[8];
+  memset(a, 0, sizeof(a));
+  int len = str.length();
+
+  for (int i = 0; i < len; ++i)
+  {
+    int p = (int)str[i];
+    //index position
+    int ip = p/32;
+    //bit position
+    int bp = p%32;
+
+    if(a[ip] & (1 << bp)) return false;
+    a[ip] |= 1 << bp;
+  }
+  return true;
+}
+
+
+//if only a-z
+ 
+bool ipUnique3(string str)
+{
+  int b = 0;
+  int len = str.length();
+
+  for (int i = 0; i < len; ++i)
+  {
+    int p = (int)str[i];
+    int bp = p % 32;
+
+    if(b & (1 << bp)) return false;
+    b |= 1 << bp;
+  }
   return true;
 }
 
 int main(int argc, char const *argv[])
 {
-  string str = "abce";
-  if(isUnique(str)) {
-    cout<< "there's no repitition" << endl;
-  } else {
-    cout<< "there's repitition" << endl;
+  string str;
+  while(1){
+    cin >> str;
+    if(ipUnique3(str)) {
+      cout<< "no repitition" << endl;
+    } else {
+      cout<< "have repitition" << endl;
+    }
   }
-
-
     
   return 0;
 }
