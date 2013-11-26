c108594e5036a45261045e83005819f0628cadf9
Daniel-Xu <xwd.forever@gmail.com>
2013-11-26 03:30:03 -0500

    recursive version
    double pointer is very important, it acts as return parameter

--- exec/2_7.cc
+++ exec/2_7.cc
@@ -2,6 +2,7 @@
 #include <stack>
 
 using namespace std;
+#define N 4 
 
 
 typedef struct node {
@@ -73,15 +74,48 @@ bool isPalindrome(link a)
   return true;
 }
 
+// I think link* is a return pointer.
+// why the return parameter's type is link*, rather than link
+// because the next value is local variable in the current stack
+// so if you want to return it, you should return its adress.
+bool isPalindromeRecurse(link a, int len, link *next)
+{
+  if(a == NULL || len == 0){  //invalid link list
+    return false; 
+  }else if(len == 1) { // length != 0 && a != NULL, odd length
+    *next = a->next;
+    return true; 
+  }else if(len == 2) { // even length
+    *next = a->next->next;
+    return a->val == a->next->val; 
+  }
+  
+  // this is the middle stack return val
+  bool res = isPalindromeRecurse(a->next, len-2, next);
+  
+  // this is stack return, determine the front and end
+  // end address is in the next 
+  // front is the head, i.e. a
+  if(res == false) {
+    return res;
+  } else {
+    res = a->val == (*next)->val;
+    *next = a->next;
+    return res;
+  }
+}
+
+
 int main(int argc, char const *argv[])
 {
   
-  int a[] = {1};
-  link head = init(a,1);
+  int a[] = {2, 2,2, 1};
+  link head = init(a,N);
   cout << "the link is:" << endl;
   print_link(head);
 
-  if (isPalindrome(head))
+  link next;
+  if (isPalindromeRecurse(head, N, &next))
     cout << "yes" << endl;
   else
     cout << "no" << endl;
