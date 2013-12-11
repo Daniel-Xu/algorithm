06d5af5ea6d4518d4d81352f0c204bf40f9cb3a2
Daniel-Xu <xwd.forever@gmail.com>
2013-12-10 20:49:05 -0500

    3_7 hasn't finished

--- exec/1_3.cpp
+++ exec/1_3.cpp
@@ -7,59 +7,59 @@ using namespace std;
 
 bool isPer1(string str1, string str2)
 {
-  if (str1.length() != str2.length())
-    return false;
-  
-  //quick sort two string
-  sort(str1.begin(), str1.end());
-  sort(str2.begin(), str2.end());
- 
-  if (str1 == str2 ) 
-    return true;
-  else
-    return false;
+    if (str1.length() != str2.length())
+        return false;
+
+    //quick sort two string
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+
+    if (str1 == str2 ) 
+        return true;
+    else
+        return false;
 }
 
 bool isPer2(string str1, string str2)
 {
-  if (str1.length() != str2.length())
-    return false;
+    if (str1.length() != str2.length())
+        return false;
 
-  int c[256];
-  memset(c, 0, sizeof(c));
-  
-  int len = str1.length();
-  for (int i = 0; i < len; ++i)
-  {
-    ++c[(int)str1[i]];
-    --c[(int)str2[i]];
-  }
-  
-  for (int i = 0; i < 256; ++i)
-  {
-    if(c[i] != 0)
-      return false;
-  }
-  
-  return true;
+    int c[256];
+    memset(c, 0, sizeof(c));
+
+    int len = str1.length();
+    for (int i = 0; i < len; ++i)
+    {
+        ++c[(int)str1[i]];
+        --c[(int)str2[i]];
+    }
+
+    for (int i = 0; i < 256; ++i)
+    {
+        if(c[i] != 0)
+            return false;
+    }
+
+    return true;
 }
 
 
 int main(int argc, char const *argv[])
 {
-  
-  string str1, str2;
-  cout << "please input string1" << endl;
-  cin >> str1;
-  cout << "please input string2" << endl;
-  cin >> str2; 
-  
-  
-  if(isPer2(str1, str2)) 
-    cout << "yes" << endl;
-  else
-    cout << "no" << endl;
-    
-  return 0;
+
+    string str1, str2;
+    cout << "please input string1" << endl;
+    cin >> str1;
+    cout << "please input string2" << endl;
+    cin >> str2; 
+
+
+    if(isPer2(str1, str2)) 
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+
+    return 0;
 }
 
---
+++ exec/3_7.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+template <typename T>
+class Animal
+{
+  public:  
+    Animal(){}
+    ~Animal(){}
+
+    //enquue
+    void 
+
+
+  private:
+    list<T> dog, cat;
+
+}
+
+class Dog: public Animal
+{
+
+}
+
+class Cat: public Animal
+{
+
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+  return 0;
+}
