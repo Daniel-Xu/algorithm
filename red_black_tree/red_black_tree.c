1237db0669ca469a7fb59d66cf95c2d4e91f2666
Daniel-Xu <xwd.forever@gmail.com>
2014-01-14 16:10:40 -0500

    left rotation and right rotation
    rotation shoud change the sucessor too.
    
    basically there's three scenarios should be considered
    
    1. x.parent with y
    
    2. y.parent with x.parent
    
    3. x.left with y.right or x.right with y.left

--- red_black_tree/red_black_tree.c
+++ red_black_tree/red_black_tree.c
@@ -1,4 +1,4 @@
-#include "binary_search_tree.h"
+#include "red_black_tree.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,151 +13,65 @@ static link make_node(int val)
 
 tree *new_tree(void)
 {
-    tree *bst = malloc(sizeof(tree));
-    bst->root = NULL;
-    return bst;
+    tree *rb_tree = malloc(sizeof(tree));
+    rb_tree->root = NULL;
+    return rb_tree;
 }
 
-void bst_insert(tree *bst, int i)
-{
-    link n = make_node(i);
 
-    link c_parent = NULL;
-    link current = bst->root;
-    
-    //move to the deepest node
-    while(current != NULL) {
-        c_parent = current; 
-        if (current->val > i) {
-            current = current->left;
-        } else {
-            current = current->right; 
-        }
-    }
 
-    //point to parent
-    n->parent = c_parent;
-    
+//two situation need to be considered
+//remember that the root's parent will be set null in insert
+//first: x is the root 
+//second: the child is nil, because nil.parent will cause segf
+void left_rotation(tree *rb, link x) {
+    //we aim at left rotation, so x.right must exist
+    link y  = x.right;
 
-    //parent decides how to point to child
-    if(c_parent == NULL) {
-        bst->root = n; 
-    } else if(c_parent->val > n->val){
-        c_parent->left = n;
-    } else {
-        c_parent->right = n; 
-    }
+    //handle y's parent and y 
+    y.parent = x.parent;
+    if (x.parent == NULL)
+        rb->root = y;
+    else if(x.parent.left == x)
+        x.parent.left = y;
+    else 
+        x.parent.right = y;
+
+    //handle y.left and x
+    x.right = y.left;
+    if(y.left != NULL)
+        y.left.parent = x;
+
+    //handle x.parent and y
+    x.parent = y;
+    y.left = x;
 }
 
-void transplant_subtree(tree *bst, link u, link v)
-{
-    // u is root
-    if(u->parent == NULL) {
-        bst->root = v;        
-    } else if( u->parent->left == u){
-        //u is left child
-        u->parent->left = v;
-    } else {
-        //u is right child 
-        u->parent->right = v;
-    }
+void right_rotation(tree *rb, link x) { 
+    link y = x.left;
 
-    //make sure v can point to parent
-    //make sure that we consider that v can be null
-    if(v != NULL) {
-        v->parent = u->parent; 
-    }
-}
+    //handle y and x.parent
+    y.parent = x.parent;
+    if(x.parent == NULL)
+        rb->root = y;
+    else if(x.parent.left == x)
+        x.parent.left = y;
+    else
+        x.parent.right = y;
 
-void bst_delete(tree *bst, int i)
-{
-    link s = bst_search(bst, i);
-    if (s == NULL) {
-        return; 
-    }
-
-    // have this node, so you can delete it
-    if(s->left == NULL) {
-        //this means left is null, so right maybe null or may not null
-        //but the situation is the same because in transplant we consider
-        //the situation v is null. 
-        transplant_subtree(bst, s, s->right);
-    } else if(s->right == NULL) {
-        // s->left != NULL, and s->right == NULL
-        transplant_subtree(bst, s, s->left);
-    } else {
-        // s have two child
-        // and the successor is in the right child subtree, of course 
-        // the successor has no left child, but could has right subtree.
         
-        link successor = bst_minimum(s->right);
+    //handle the x.left with y.right
+    x.left = y.right;
+    if(y.right != NULL)
+        y.right.parent = x;
 
-        //successor is the direct right child of it.
-        if(s->right != successor) {
-            transplant_subtree(bst, successor, successor->right);         
-
-            //handle s->right, it's double link
-            successor->right = s->right;
-            successor->right->parent = successor;
-        }
-
-        transplant_subtree(bst, s, successor);
-        successor->left = s->left;
-        successor->left->parent = successor;
-    }
-
-    free(s);
+    //handle the x.parent with y
+    x.parent = y;
+    y.right = x;
 }
 
-//node successor
-link bst_successor(link x)
-{
-    if(x->right != NULL) {
-        return bst_minimum(x->right);
-    }
 
-    //no right child, just find the parent
-    link y = x->parent;
-    while (y != NULL && x == y->right) {
-        x = y; 
-        y = y->parent;
-    }
 
-    return y;
-}
-
-//minimum
-link bst_minimum(link x)
-{
-    while(x->left != NULL) {
-        x = x->left;
-    }
-    return x;
-}
-
-//maximum
-link bst_maximum(link x)
-{
-    while(x->right != NULL)
-        x = x->right;
-
-    return x;
-}
-
-//search
-link bst_search(tree *bst, int i)
-{
-    link current = bst->root;
-    while(current != NULL && i != current->val ){
-        if(current->val > i) {
-            current = current->left; 
-        } else {
-            current = current->right; 
-        }
-    }
-
-    return current;
-}
 
 
 //pre order
--- red_black_tree/red_black_tree.h
+++ red_black_tree/red_black_tree.h
@@ -1,8 +1,11 @@
 #ifndef RED_BLACK_TREE_H_XEX90NU3
 #define RED_BLACK_TREE_H_XEX90NU3RED_BLACK_TREE_H_XEX90NU3
 
+enum color_type { RED, BLACK };
+
 typedef struct node{ 
   int val;
+  enum color_type color;
   struct node *parent;
   struct node *left;
   struct node *right;
@@ -12,26 +15,9 @@ typedef struct tree {
     link root;
 }tree;
 
-
-
 //declaration
 
 tree *new_tree(void);
-
-void bst_insert(tree *bst, int i);
-
-void transplant_subtree(tree *bst, link u, link v);
-
-void bst_delete(tree *bst, int i);
-
-link bst_minimum(link x);
-
-link bst_successor(link x);
-
-link bst_maximum(link x);
-
-link bst_search(tree *bst, int i);
-
 void pretty_print(link p, int indent);
 
 
