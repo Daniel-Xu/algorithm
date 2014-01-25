4de68b82ae5b59d6531276f66e9b7cb6eb32917d
Daniel-Xu <xwd.forever@gmail.com>
2014-01-24 23:18:40 -0500

    recursive version
    recursive version should think the different way.
    
    you should think about when to abort, i.e. the base statement

--- exec/9_3_magic_index.cc
+++ exec/9_3_magic_index.cc
@@ -3,25 +3,40 @@
 using namespace std;
 //if a[i] > i, then a[x] > x if x>i
 //if a[i] < i, then a[x] < x if x<i
-int magic_index(int a[], int start, int end)
-{
-    while(start <= end){
-        int mid = (start+end)/2;
-        if(a[mid] > mid)
-            end = mid-1;
-        else if(a[mid] < mid)
-            start = mid+1;
-        else
-            return mid;
-    }
+//int magic_index(int a[], int start, int end)
+//{
+    //while(start <= end){
+        //int mid = (start+end)/2;
+        //if(a[mid] > mid)
+            //end = mid-1;
+        //else if(a[mid] < mid)
+            //start = mid+1;
+        //else
+            //return mid;
+    //}
 
-    return -1;
+    //return -1;
+//}
+
+int magic_index(int a[], int start, int end, int len)
+{
+    if(start < 0 || end >= len || start > end)
+        return -1;
+
+    int mid = (start+end)/2;
+
+    if(a[mid] > mid)
+        return magic_index(a, start, mid-1,len);
+    else if(a[mid] < mid)
+        return magic_index(a, mid+1, end, len);
+    else 
+        return mid;
 }
 
 int main(int argc, char const *argv[])
 {
     int a[] = {-1, 1, 3, 4,5};
 
-    cout << magic_index(a, 0, 4) <<endl;
+    cout << magic_index(a, 0, 4, 5) <<endl;
     return 0;
 }
