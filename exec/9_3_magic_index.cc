36a6c07d8c5f4e490c92eb0de8d094489b388d96
Daniel-Xu <xwd.forever@gmail.com>
2014-01-24 21:41:56 -0500

    this is like binary search
    and this is non-recursion version

---
+++ exec/9_3_magic_index.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+//if a[i] > i, then a[x] > x if x>i
+//if a[i] < i, then a[x] < x if x<i
+int magic_index(int a[], int start, int end)
+{
+    while(start <= end){
+        int mid = (start+end)/2;
+        if(a[mid] > mid)
+            end = mid-1;
+        else if(a[mid] < mid)
+            start = mid+1;
+        else
+            return mid;
+    }
+
+    return -1;
+}
+
+int main(int argc, char const *argv[])
+{
+    int a[] = {-1, 1, 3, 4,5};
+
+    cout << magic_index(a, 0, 4) <<endl;
+    return 0;
+}
